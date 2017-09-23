f2ef381a4b8fccda66b8bf4a6596787e10092562
Tatiana Karpova <tatkirkar@gmail.com>
2017-09-24 00:25:58 +0300

    add for Alyona with love

--- reciever.c
+++ reciever.c
@@ -3,25 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define char_number  20
+const int CHAR_NUMBER = 2;
+const int BITS_PER_CHAR = 8;
 
 int main () {
         int trans_pid = 0;
         int rec_pid = 0;
 	int i = 0;
 	int j = 0;
-	char msg[char_number];
-	char msg_bin_letter[8];
+	char msg[CHAR_NUMBER];
+	char msg_bin_letter[BITS_PER_CHAR];
+
         rec_pid = getpid ();
-        printf ("my pid is %d, i am a reciever", rec_pid);
+        printf ("my pid is %d, i am a reciever\n", rec_pid);
         scanf ("%d", &trans_pid);
-        printf ("the transmitter's pid id %d", trans_pid);
+        printf ("the transmitter's pid id %d\n", trans_pid);
+
+
+	for (i = 0; i < CHAR_NUMBER; msg[i] = '\0', i++);
+
+        for (i = 0; i < CHAR_NUMBER; i++){
+	        msg[i] = getchar();
+	}
 
 		
-	for (i = 0; i < char_number; i++){
-		for (j = 0; j < 8; j++){
+	for (i = 0; i < CHAR_NUMBER; i++){
+		for (j = 0; j < BITS_PER_CHAR; j++){
 			msg_bin_letter[j] = msg[i] % 2;
-			msg[i] = msg[i] >> 1;
+			msg[i] >>= 1;
 			printf("%d", msg_bin_letter[j]); 
 		}
 				
