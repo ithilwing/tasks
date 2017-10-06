352a1d0525909b914686be70b16e3c848a0c0150
Tatiana Karpova <tatkirkar@gmail.com>
2017-10-06 15:14:18 +0300

    Calculations in threads

--- thread.c
+++ thread.c
@@ -1,3 +1,5 @@
+//Kapova, Smirnov and an independent expert 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -5,47 +7,88 @@
 #include <unistd.h>
 
 
-int j = 0;
-int n = 0;
-int counter = 0;
-long long int current_sum[n + 1];
-
-struct params{
-	int left;
-	int right;
-};
+typedef struct{
+	long int left;
+	long int right;
+	long long int sum;
+}params;
 
 void* my_func(void *pointer){
-	int local_l, local_r;
 	params* x;
 	x = (params*)pointer; // x is the pointer for params
-	local_l = x->left;
-	local_r = x->right;
+	int k = x->left;
+	x->sum = 0;
+	for (k = x->left; k <= x->right; k++){
+//		printf("%d\n", x->left);
+		x->sum = x->sum + k;
+	}
+	return x;
 
 	
 }
 
 int main () {
-	int right, left;
+	long int L, R;
+	int n;
 	int i = 0;
-	int fail = 0;
-	int pack = 0;
-	int rest = 0;
-	scanf("%d\n", &L);
-	scanf("%d\n", &R);
-	scanf("%d\n", &n);
-	int j = L;
-	digits_in_pack = (R - L)/(n);
+	int thread_result = 0;
+	long long int final_result = 0;
+	int digits_in_pack = 0;
+	int last_left;
+	printf("Enter left border: ");
+	scanf("%ld", &L);
+        printf("Enter right border: ");
+	scanf("%ld", &R);
+        printf("Enter number of threads: ");
+	scanf("%d", &n);
+
+	digits_in_pack = (R - L + 1)/(n);
+//	printf("\n%d digits in pack\n",digits_in_pack );
+	
+
+	params* structures = (params*) malloc(sizeof(params) * n);
+	pthread_t* thread_id = malloc(sizeof(pthread_t) * n);
+
+
 	for (i = 0; i < n; i++){
-		fail = pthread_create(&thread_id,
-				(pthread_attr_t *)NULL,
+		
+		structures[i].left = L + (i * digits_in_pack);
+		structures[i].right = structures[i].left + digits_in_pack - 1;
+	//	printf("%d structures[%d].left and ", structures[i].left, i);
+        //        printf("%d structures[%d].right\n", structures[i].right, i);	
+		
+		thread_result = pthread_create(&thread_id[i],
+				NULL,
 				my_func,
-				(void *)&thread_id
+				(params*)&structures[i]
 				);
-		if (fail) {
-			printf("can't create thread, returned value = %d\n", fail);
+		if (thread_result) {
+			printf("can't create thread, returned value = %d\n", thread_result);
+			exit(0);
 		}
+	//	structures[i].left = structures[i].left + 1;
+
 	}
+
+	for (i = 0; i < n; i++){
+		pthread_join(thread_id[i], NULL);
+		final_result = final_result + structures[i].sum;
+	}
+//	int digits_left = R -  (( R - L + 1) % n);
+	last_left = L + (digits_in_pack * n);
+//	int in_last_pack = R - (last_left) + 1
+//	printf("last_left = %d and digits left = %\n", last_left, digits_left);
+	for (i = last_left; i <= R; i++){
+		final_result = final_result + i;
+
+	}
+
+	printf("Congratulations!\nThe sum from %ld to %ld is %lld\n",L, R, final_result);
+
+	free(structures);
+	free(thread_id);
+
+	
 
 	return 0;
 
