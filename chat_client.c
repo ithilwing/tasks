fc8320970477d8f211f3ee7cfba7e2227c98d59b
danilgrig <danilgrig@gmail.com>
2017-12-02 18:28:13 +0300

    update

--- chat_client.c
+++ chat_client.c
@@ -15,6 +15,9 @@
 #include <string.h>
 #include <pthread.h>
 
+#define NMAX 300
+
+char buffer_from_server[NMAX];
 /*пыталась сделать одновременно с посылом сообщений (в мейне) отдельным параллельным тредом тупо чтение всего, что приходит от сервера. Получается криво, но сообщения в тредах все появляются. Если не обращать внимания на please enter ur msg в неожиданных местах и мусора. Но надо исправлять */
 
 /* НАСЧЁТ МУСОРА - это должно исправиться, если учитывать как символы то ли пробел, то ли энтер. Но у меня уже нет времени */
@@ -29,15 +32,18 @@ void error (char *msg){
 void* message_reading (void *args){ // закинули аргументом файлдескриптор слиента
 
 	while(1){
-	
-	int * new_args = (int*)args;
-	int newsockfd = *new_args;
-	char buffer_from_server[256];
-	int n;
-	n = read(newsockfd, buffer_from_server, 255); // прочитали всё что есть в буфере
-	printf("%s\n", buffer_from_server); // вывели
-	if (n < 0)
-		error("ERROR reading from socket");
+		int * new_args = (int*)args;
+		int newsockfd = *new_args;
+//		char buffer_from_server[256];
+		int n;
+		if((n = read(newsockfd, buffer_from_server, 255)) <= 0){
+			printf("connection lost\n");
+			exit(0);
+			}		 // прочитали всё что есть в буфере
+		printf("%s\n", buffer_from_server); // вывели
+		if (n < 0) {
+			error("ERROR reading from socket");
+		}
 	}
 }
 
@@ -49,7 +55,7 @@ int main(int argc, char *argv[]){
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 
-	char buffer[256];
+	char buffer[NMAX];
 	if (argc < 3){
 		fprintf(stderr, "usage %s hostname port\n", argv[0]);
 		exit(0);
@@ -85,13 +91,14 @@ int main(int argc, char *argv[]){
 /*Вот тут короче надо просто повозиься с корректным отображением pls enter ur msg */
 
         while(1){
+		sleep(1);
+		printf("Please enter the message: \n");
+		scanf("%s", buffer);
 
-		printf("Please enter the message: ");
+		//bzero(buffer, 256);
+		//fgets(buffer, 255, stdin);
 
-		bzero(buffer, 256);
-		fgets(buffer, 255, stdin);
-
-		n = write(sockfd, buffer, strlen(buffer));
+		n = write(sockfd, buffer, strlen(buffer)+1);
 
 
 		if (n < 0)
